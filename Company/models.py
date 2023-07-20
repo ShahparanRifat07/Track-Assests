e1afc34e93084f0abc83fe3ac1de0cfc54768ec7
Shahparan Rifat <shahranrifat115207@gmail.com>
2023-07-20 23:50:40 +0600

    Added Company Model

--- Company/models.py
+++ Company/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.utils import timezone
+from Account.models import User
 
 # Create your models here.
+
+
+class Company(models.Model):
+    name = models.CharField(max_length=128,blank=False)
+    company_admin = models.OneToOneField(User, on_delete=models.CASCADE)
+    email = models.EmailField(max_length=128)
+    created_at = models.DateTimeField(default=timezone.now)
+    updated_at = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return f"{self.name}--{self.company_admin.username}"
--- Core/settings.py
+++ Core/settings.py
@@ -38,6 +38,13 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+
+    #Custom Apps
+    'Account',
+    'Company',
+    'Employee',
+    'Device',
 ]
 
 MIDDLEWARE = [
